ba28b00dd8a0ab95e20d8d719d87ce14b7cf7b7c
root <jarrett_jewart@yahoo.com>
2022-05-03 19:53:44 -0700

    Q3 retry

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number < 0:
+if (number < 0):
     last_digit = ((number * -1) % 10) * -1
 else:
     ast_digit = number % 10
-    if last_digit == 0:
+    if (last_digit == 0):
         print("Last digit of {} is {} and is 0".format(number, last_digit))
     elif last_digit > 5:
         print("Last dogot of {} is {}".format(number, last_digit), end=" ")
