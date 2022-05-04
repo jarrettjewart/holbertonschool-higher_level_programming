c1f4d43329b5372cd97d78ec5765ea8b6b4cf41f
root <jarrett_jewart@yahoo.com>
2022-05-03 20:16:47 -0700

    Q5v3

--- 0x00-python-hello_world/5-print_comb2.py
+++ 0x00-python-hello_world/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for n in range(0, 100):
     if n <= 98:
-        print("{:02}, ".format(n), end= "")
+        print("{:02}, ".format(n), end="")
     else:
         print("{:02}".format(n))
