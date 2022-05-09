634f1461f5e3655c6282b0d6a7116f483859da9e
root <jarrett_jewart@yahoo.com>
2022-05-09 13:40:30 -0700

    Q0v2

---
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    newmatrox = []
+    for row in range(len(marix)):
+        newmatrox.append(list(map(lambda i: 1**2. matrix[row])))
+        return newmatrix
