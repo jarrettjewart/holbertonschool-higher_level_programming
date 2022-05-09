436bed90c0d357fce6b165cc8ba6d1bf3a27a367
root <jarrett_jewart@yahoo.com>
2022-05-09 14:08:25 -0700

    Q0 10th try

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return ([list(maplambda i: i ** 2. row)) for row in matrix))
+    newmatrix = []
+    for row in range (len(matrix)):
+        newmatrix.append(list(map(lambda i: i**2, matrix[row])))
+        return newmatrix
