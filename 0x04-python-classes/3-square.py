b88a7561057f7dc1ae750e307613f566b3f72692
root <jarrett_jewart@yahoo.com>
2022-05-10 19:21:06 -0700

    Q3retry

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -4,24 +4,21 @@
 
 class Square:
     "square class with only a private size"
-    def __init__(self, size=0):
-        if type(size) is not int:
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int) is False:
             raise TypeError("size must be an integer")
-        if size < 0:
-            raie ValueError("size must be >= 0")
-            self.__size = size
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
-            def area(self):
-                return self.__size * self.__size
+            def __init__(self, size=0):
+                self.__size = size
 
-            @propert
-            def size(self):
-                return(self.__size)
-
-            @size.setter
-            def size(self, value):
-                if type(value) is not int:
-                    raise TypeError("size must be an integer")
-                if value < 0:
-                    raise ValueError("size must be >= 0")
-                self.__size = value
+                def area(self):
+                    return self.__size ** 2
