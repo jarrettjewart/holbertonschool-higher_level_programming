5f0414bebf861e460fc99714ffe2c73dcf6131b5
root <jarrett_jewart@yahoo.com>
2022-05-10 18:59:44 -0700

    Q2v5

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     "square class with only a private size"
-    def __init__(self, siz=0):
+    def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
