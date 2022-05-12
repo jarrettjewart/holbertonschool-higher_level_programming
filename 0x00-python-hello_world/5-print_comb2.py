0e9f82bd968f8dfb8502c4db739cec344685823a
root <jarrett_jewart@yahoo.com>
2022-05-12 09:54:20 -0700

    Q5revmap

--- 0x00-python-hello_world/5-print_comb2.py
+++ 0x00-python-hello_world/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for n in range(0, 100):
-    if n <= 98:
-        print("{:02}, ".format(n), end="")
-    else:
-        print("{:02}".format(n))
+    if n <= 99:
+        print("{:02}".format(i), end=', ')
+    elif i == 99:
+        print(i)
