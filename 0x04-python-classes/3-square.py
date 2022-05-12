4555488100920361375cdf4344b2a8c6f78e8d02
root <jarrett_jewart@yahoo.com>
2022-05-11 19:30:43 -0700

    Q3ugh

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -4,21 +4,24 @@
 
 class Square:
     "square class with only a private size"
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
     @property
     def size(self):
-        return self.__size
+        return(self.__size)
 
     @size.setter
     def size(self, value):
-        if isinstance(value, int) is False:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-
-            def __init__(self, size=0):
-                self.__size = size
-
-                def area(self):
-                    return self.__size ** 2
+        self.__size = value
