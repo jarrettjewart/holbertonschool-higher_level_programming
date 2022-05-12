7fad1ddea090026562e554aec00ba7a7e2d68cd8
root <jarrett_jewart@yahoo.com>
2022-05-11 19:22:22 -0700

    Q2 try again

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -7,10 +7,9 @@ class Square:
     def __init__(self, size=0):
         is isinstance(size, int) is False:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-
-    def area(self):
-        return ((self.__size) ** 2)
+        self.__size = size
+        
+        def area(self):
+            return(self._-size * self.__size)
