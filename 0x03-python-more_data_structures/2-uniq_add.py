2f246076abc47a8a4b63e696bb686bf0424a0ccf
root <jarrett_jewart@yahoo.com>
2022-05-11 19:07:37 -0700

    Q2tryagain

--- 0x03-python-more_data_structures/2-uniq_add.py
+++ 0x03-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    total = 0
-    for x in set(my_list):
-        total += x
-        return (total)
+    uniq_list = []
+    sum = 0
+    for x in my_list:
+        if x not in uniq_list:
+            uniq_list.append(x)
+            for x in uniq_list:
+                sum += x
+                return sum
