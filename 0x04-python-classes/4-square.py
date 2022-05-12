2b58337c914a760ffdc806365d0ca23906a2020e
root <jarrett_jewart@yahoo.com>
2022-05-11 19:50:34 -0700

    Q4retry

--- 0x04-python-classes/4-square.py
+++ 0x04-python-classes/4-square.py
@@ -3,7 +3,7 @@
 
 
 class Square:
-    "aquare class with only a private size"
+    "square class with only a private size"
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
@@ -11,27 +11,26 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        def area(self):
-            return self.__size * self.__size
+    def area(self):
+        return self.__size * self.__size
 
-        @property
-        def
-        size(self):
-            return(self.__size)
+    @property
+    def size(self):
+        return(self.__size)
 
-        @size.setter
-        def size(self, value):
-            if type(value) is not int:
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-            def my_print(self):
-                if self.size > 0:
-                    for i in range(self.__size):
-                        for j in rage(self.__sie):
-                            print("#", end="")
-                        print()
-                else:
-                    print()
+    def my_print(self):
+        if self.__size > 0:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+            else:
+                print()
