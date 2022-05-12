77e2580113a6dceced5c3f834103a05e2dee4d4e
root <jarrett_jewart@yahoo.com>
2022-05-11 19:57:25 -0700

    Q4 try again

--- 0x04-python-classes/4-square.py
+++ 0x04-python-classes/4-square.py
@@ -5,18 +5,14 @@
 class Square:
     "square class with only a private size"
     def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        sefl.__size = size
 
     def area(self):
-        return self.__size * self.__size
+        return self.__size ** 2
 
     @property
     def size(self):
-        return(self.__size)
+        return self.__size
 
     @size.setter
     def size(self, value):
@@ -27,10 +23,10 @@ class Square:
         self.__size = value
 
     def my_print(self):
-        if self.__size > 0:
-            for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#", end="")
-                print()
-            else:
+        if self.__size) == 0:
+            print()
+        else:
+            for i in range(slef.__size):
+                for i in range(self.__self):
+                    print('#', end="")
                 print()
