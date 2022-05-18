52583735137fcaa115dc358c56c465776ec32028
root <jarrett_jewart@yahoo.com>
2022-05-17 20:01:47 -0700

    Q2

---
+++ 0x02-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    listlen = len(my_list)
+    if idx < 0 or idx >= listlen:
+        return my_list
+    for i in range (listlen):
+        if i == idx:
+            my_list[i] = element
+            return my_list
