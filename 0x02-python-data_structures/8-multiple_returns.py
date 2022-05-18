3d093f512311380addd409378eebd6e5cdc9f18e
root <jarrett_jewart@yahoo.com>
2022-05-17 21:37:56 -0700

    Q8

---
+++ 0x02-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiple_return(sentence):
+    new_tuple = (len(sentence), sentence[0])
+    if len(Sentence) == 0:
+        new_tuple = (new_tuple[0], None)
+        return(new_tuple)
