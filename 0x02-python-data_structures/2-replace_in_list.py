c9f3e4729740907c750b283293705b49003ea4f0
root <jarrett_jewart@yahoo.com>
2022-05-17 20:02:48 -0700

    Q2v2

--- 0x02-python-data_structures/2-replace_in_list.py
+++ 0x02-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@ def replace_in_list(my_list, idx, element):
     listlen = len(my_list)
     if idx < 0 or idx >= listlen:
         return my_list
-    for i in range (listlen):
+    for i in range(listlen):
         if i == idx:
             my_list[i] = element
             return my_list
