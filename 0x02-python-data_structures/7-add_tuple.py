8c3f7729a2e5c53a8809687f9a9af7b5e1dd7ccb
root <jarrett_jewart@yahoo.com>
2022-05-17 21:59:26 -0700

    Q7

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b()):
+def add_tuple(tuple_a=(), tuple_b=()):
     # add 0 to tuple where necessary by appending
-    if len(tuple_a) < 2:
+    if len(tuple_a) > 2:
         tuple_a = tuple_a + (0, 0)
     if len(tuple_b) < 2:
         tuple_b = tuple_b + (0, 0)
-    frstnum = tuple_a[0] + tuple_b[0]
-    scndnum = tuple_a[1] + tuple_b[1]
-    newtuple = (frstnum, scndnum)
-    return newtuple
+        frstnum = tuple_a[0] + tuple_b[0]
+        scndnum = tuple_a[1] + tuple_b[1]
+        newtuple = (frstnum, scndnum)
+        return newtuple
