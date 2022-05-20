e3e8581182d56d2d7b283eab86b2165766ce7aea
root <jarrett_jewart@yahoo.com>
2022-05-20 08:38:42 -0700

    Q0 v4

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -18,4 +18,6 @@ def add_integer(a b=98):
         raise TypeError("a must be an integer")
     if type(b) is not int:
         raise TypeError("b must be an integer")
+    if a is None or b is None:
+        return
     return a + b
