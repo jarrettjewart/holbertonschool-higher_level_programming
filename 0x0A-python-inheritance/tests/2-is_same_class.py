0422573bc3c783106fbe4ffb53042db416a7e43c
jarrettjewart <92384372+jarrettjewart@users.noreply.github.com>
2023-02-27 06:49:36 -0600

    Task 2

---
+++ 0x0A-python-inheritance/tests/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+'''Task 2'''
+
+
+def is_same_class(obj, a_class):
+    '''function that returns True if the object is exactly
+    an instance of the specified class ; otherwise False.'''
+    return type(obj) is a_class
