943cd270e931c86ba6656940e9ed6b149aebe2ee
jarrettjewart <92384372+jarrettjewart@users.noreply.github.com>
2023-02-27 06:59:22 -0600

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''Task 6'''
+
+
+class BaseGeometry():
+    '''class BaseGeometry'''
+    def area(self):
+        '''Function that raises an Exception with the message area()
+        is not implemented'''
+        raise Exception("area() is not implemented")
