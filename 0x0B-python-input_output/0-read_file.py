54609b23efab84addddc41f786d71aba8398c4cc
jarrettjewart <92384372+jarrettjewart@users.noreply.github.com>
2023-02-27 07:24:48 -0600

    Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+'''Task 0'''
+
+
+def read_file(filename=""):
+    '''function that reads a text file (UTF8)
+    and prints it to stdout:'''
+    with open(filename, mode='r', encoding='utf-8') as f:
+        print(f.read(), end="")
